63f57f28079a4cc923890c1162e2cb31e6b5bd0b
Paul Granados <Paul Granados@DESKTOP-J4RED30>
2025-08-02 17:03:54 -0600

    "cambios 02-08-2025"

---
+++ src/github/GITHUB.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+import java.util.Scanner;
+/**
+ *
+ * @author Paul Granados
+ */
+public class GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String nombre,edad;
+        Scanner lector=new Scanner(System.in);
+        System.out.println("Ingrese su nombre ");
+        nombre=lector.next();
+        System.out.println("Ingrese su edad");
+        edad=lector.next();
+    }
+    
+}
